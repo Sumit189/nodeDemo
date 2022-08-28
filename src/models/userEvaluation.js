8b91e60fc42c18e6fe4809967546d205c7b968c7
sumit.paul <sumit.18.paul@gmail.com>
2022-08-29 01:38:01 +0530

    temp fix

--- src/models/userEvaluation.js
+++ src/models/userEvaluation.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 const UserEvaluations = mongoose.model('UserEvaluations',{
-    userid:{
-        type: String,
-        required: true
-    },
     question:{
         type: Number,
         required: true,
